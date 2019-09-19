5e7736aef08f4c44141006ca9e9c845a2d1f29b6
Tomas Zatko <tzatko@users.noreply.github.com>
2019-09-19 16:04:19 +0200

    Initial commit

---
+++ h.py
@@ -0,0 +1,158 @@
+#!/usr/local/bin/python3
+
+"""
+I often need some basic domain information in a clear human-readable form.
+This script provides the information I need in the form I like.
+
+saved as:
+    ~/bin/h.py
+used as:
+    alias h='~/bin/h.py'
+
+2019-09-09 Tomas Zatko https://keybase.io/wo_ody
+"""
+
+import dns.resolver,dns.reversename
+from termcolor import colored
+import sys
+
+if (len(sys.argv)>1):
+    D = str(sys.argv[1])
+else:
+    print(colored('Usage: '+sys.argv[0]+" doman.name",'red'))
+    sys.exit(-1)
+
+
+print(colored('IPv4:\n', 'green'))
+myResolver = dns.resolver.Resolver()
+
+try:
+    myAnswers = myResolver.query(D, "A")
+    for rdata in myAnswers:
+        try:
+            ip=str(rdata)
+            n = dns.reversename.from_address(ip)
+            ptrs=dns.resolver.query(n,"PTR")
+            for num in ptrs:
+                print("A:\t"+D+" -> "+str(rdata)+" -> "+str(num))
+        except:
+            print("A:\t"+D+" -> "+str(rdata)+" -> "+colored('? ? ?','red'))
+except:
+    print("A:\t"+D+" -> "+colored('? ? ?','red'))
+print("")
+
+try:
+    myAnswers = myResolver.query(D, "MX")
+    for rdata in myAnswers:
+        try:
+            myAnswersA = myResolver.query(rdata.exchange, "A")
+            for ips in myAnswersA:
+                ip=str(ips)
+                try:
+                    n = dns.reversename.from_address(ip)
+                    ptrs=dns.resolver.query(n,"PTR")
+                    for num in ptrs:
+                        print("MX: "+str(rdata.preference)+"\t"+str(rdata.exchange)+" -> "+str(ips)+" -> "+str(num))
+                except:
+                    print("MX: "+str(rdata.preference)+"\t"+str(rdata.exchange)+" -> "+str(ips)+" -> "+colored('? ? ?','red'))
+        except:
+            print("MX: "+str(rdata.preference)+"\t"+str(rdata.exchange)+" -> "+colored('? ? ?','red'))
+except:
+    print("MX: "+colored('? ? ?','red'))
+print("")
+
+
+try:
+    myAnswers = myResolver.query(D, "NS")
+    for rdata in myAnswers:
+        try:
+            myAnswersA = myResolver.query(str(rdata), "A")
+            for ips in myAnswersA:
+                ip=str(ips)
+                try:
+                    n = dns.reversename.from_address(ip)
+                    ptrs=dns.resolver.query(n,"PTR")
+                    for num in ptrs:
+                        print("NS: "+str(rdata)+" -> "+str(ips)+" -> "+str(num))
+                except:
+                    print("NS: "+str(rdata)+" -> "+str(ips)+" -> "+colored('? ? ?','red'))
+        except:
+            print("NS: "+str(rdata)+" -> "+colored('? ? ?','red'))
+except:
+    print("NS: "+colored('? ? ?','red'))
+print("\n")
+
+print(colored('IPv6:\n', 'green'))
+try:
+    myAnswers = myResolver.query(D, "AAAA")
+    for rdata in myAnswers:
+        ip=str(rdata)
+        n = dns.reversename.from_address(ip)
+        try:
+            ptrs=dns.resolver.query(n,"PTR")
+            for num in ptrs:
+                print("AAAA:\t"+D+" -> "+str(rdata)+" -> "+str(num))
+        except:
+            print("AAAA:\t"+D+" -> "+str(rdata)+" -> " + colored('? ? ?', 'red'))
+except:
+    print("AAAA:\t"+D+" -> "+colored('? ? ?','red'))
+print("")
+
+try:
+    myAnswers = myResolver.query(D, "MX")
+    for rdata in myAnswers:
+        try:
+            myAnswersA = myResolver.query(rdata.exchange, "AAAA")
+            for ips in myAnswersA:
+                ip=str(ips)
+                try:
+                    n = dns.reversename.from_address(ip)
+                    ptrs=dns.resolver.query(n,"PTR")
+                    for num in ptrs:
+                        print("MX: "+str(rdata.preference)+"\t"+str(rdata.exchange)+" -> "+str(ips)+" -> "+str(num))
+                except:
+                    print("MX: "+str(rdata.preference)+"\t"+str(rdata.exchange)+" -> "+str(ips)+" -> "+colored('? ? ?','red'))
+        except:
+            print("MX: "+str(rdata.preference)+"\t"+str(rdata.exchange)+" -> "+colored('? ? ?','red'))
+except:
+    print("MX: "+colored('? ? ?','red'))
+print("")
+
+
+try:
+    myAnswers = myResolver.query(D, "NS")
+    for rdata in myAnswers:
+        try:
+            myAnswersA = myResolver.query(str(rdata), "AAAA")
+            for ips in myAnswersA:
+                ip=str(ips)
+                try:
+                    n = dns.reversename.from_address(ip)
+                    ptrs=dns.resolver.query(n,"PTR")
+                    for num in ptrs:
+                        print("NS: "+str(rdata)+" -> "+str(ips)+" -> "+str(num))
+                except:
+                    print("NS: "+str(rdata)+" -> "+str(ips)+" -> "+colored('? ? ?','red'))
+        except:
+            print("NS: "+str(rdata)+" -> "+colored('? ? ?','red'))
+except:
+    print("NS: "+colored('? ? ?','red'))
+print("")
+
+print(colored('Other:\n', 'green'))
+
+try:
+    myAnswers = myResolver.query(D, "TXT")
+    for rdata in myAnswers:
+        print("TXT:\t"+str(rdata))
+except:
+    print("TXT:\t"+colored('? ? ?','red'))
+print("")
+
+try:
+    myAnswers = myResolver.query(D, "SOA")
+    for rdata in myAnswers:
+        print("SOA:\t"+str(rdata))
+except:
+    print("SOA:\t"+colored('? ? ?','red'))
+print("")
